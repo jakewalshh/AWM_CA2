a60ef003c4e663effb4f20b38f294d73947e5707
jakewalshh <jakejwalsh1@gmail.com>
2025-12-03 21:04:58 +0000

    Created example lorry with location. fixed error , long and lat now serialized properly

--- tracking/admin.py
+++ tracking/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
 from .models import Lorry, Location
 
 @admin.register(Lorry)
@@ -7,7 +8,7 @@ class LorryAdmin(admin.ModelAdmin):
     search_fields = ['name']
 
 @admin.register(Location)
-class LocationAdmin(admin.ModelAdmin):
+class LocationAdmin(OSMGeoAdmin):
     list_display = ['lorry', 'timestamp', 'current_county']
     list_filter = ['timestamp', 'current_county']
     readonly_fields = ['timestamp']
--- tracking/serializers.py
+++ tracking/serializers.py
@@ -8,7 +8,15 @@ class LorrySerializer(serializers.ModelSerializer):
 
 class LocationSerializer(serializers.ModelSerializer):
     lorry_name = serializers.CharField(source='lorry.name', read_only=True)
+    latitude = serializers.SerializerMethodField()
+    longitude = serializers.SerializerMethodField()
     
+    def get_latitude(self, obj):
+        return obj.point.y if obj.point else None
+
+    def get_longitude(self, obj):
+        return obj.point.x if obj.point else None
+
     class Meta:
         model = Location
-        fields = ['id', 'lorry', 'lorry_name', 'point', 'timestamp', 'current_county']
+        fields = ['id', 'lorry', 'lorry_name', 'latitude', 'longitude', 'timestamp', 'current_county']
